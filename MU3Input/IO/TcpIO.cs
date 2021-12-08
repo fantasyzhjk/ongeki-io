1dfa04631cd2cc1efd66c8a9fd2322c524507441
Sanhei <w953934508@outlook.com>
2021-12-08 19:28:34 +0800

    允许在segatools.ini中修改监听端口

--- MU3Input/IO/TcpIO.cs
+++ MU3Input/IO/TcpIO.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,7 +11,7 @@ namespace MU3Input
 {
     public class TcpIO : IO
     {
-        const int port = 4354;
+        const int defaultPort = 4354;
         TcpListener listener;
         TcpClient client;
         NetworkStream networkStream;
@@ -17,19 +19,35 @@ namespace MU3Input
         {
             _data = new OutputData() { Buttons = new byte[10], AimiId = new byte[10] };
             IPAddress ip = new IPAddress(new byte[] { 0, 0, 0, 0 });
-            listener = new TcpListener(ip, port);
+            listener = new TcpListener(ip, GetPort());
             listener.Start();
             new Thread(PollThread).Start();
-            //接收多个连接并保留最后一个
-            new Thread(async() =>
+            //接收多个连接并只保留最后一个
+            new Thread(async () =>
             {
-                while (true) 
+                while (true)
                 {
                     connectTask = ConnectAsync();
                     await connectTask;
                 }
             }).Start();
 
+        }
+        public int GetPort()
+        {
+            var location = GetType().Assembly.Location;
+            string directoryName = Path.GetDirectoryName(location);
+            string segatoolsIniPath = Path.Combine(directoryName, "segatools.ini");
+            if (File.Exists(segatoolsIniPath))
+            {
+                StringBuilder temp = new StringBuilder();
+                GetPrivateProfileString("mu3io", "port", defaultPort.ToString(), temp, 1024, segatoolsIniPath);
+                if(int.TryParse(temp.ToString(),out int port))
+                {
+                    return port;
+                }
+            }
+            return defaultPort;
         }
         Task connectTask;
         public override bool IsConnected => client?.Connected ?? false;
@@ -89,24 +107,16 @@ namespace MU3Input
 
         public override unsafe void SetLed(uint data)
         {
-            if (!IsConnected)
+            try
+            {
+                if (!IsConnected)
+                    return;
+                networkStream.Write(BitConverter.GetBytes(data), 0, 4);
+            }
+            catch
+            {
                 return;
-
-            //SetLedInput led;
-            //led.Type = 0;
-            //led.LedBrightness = 40;
-
-            //for (var i = 0; i < 9; i++)
-            //{
-            //    led.LedColors[i] = (byte)(((data >> bitPosMap[i]) & 1) * 255);
-            //    led.LedColors[i + 15] = (byte)(((data >> bitPosMap[i + 9]) & 1) * 255);
-            //}
-
-            //var outBuffer = new byte[64];
-            //fixed (void* d = outBuffer)
-            //    CopyMemory(d, &led, 64);
-            ;
-            networkStream.Write(BitConverter.GetBytes(data), 0, 4);
+            }
         }
         public override unsafe void SetAimiId(byte[] id)
         {
@@ -125,6 +135,8 @@ namespace MU3Input
 
             networkStream.Write(outBuffer, 0, outBuffer.Length);
         }
+        [DllImport("kernel32")]//返回取得字符串缓冲区的长度
+        private static extern long GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
         [DllImport("kernel32.dll", EntryPoint = "CopyMemory", SetLastError = false)]
         private static extern unsafe void CopyMemory(void* dest, void* src, int count);
     }
