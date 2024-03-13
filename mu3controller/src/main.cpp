554a7cbc506885cf3a767c470581a2c997285dd9
fantasyzhjk <fantasyzhjk@outlook.com>
2024-03-13 21:23:08 +0800

    删除

--- mu3controller/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdinclude.hpp"
-
-void setup() {
-    component::manager::start();
-}
-
-void loop() {
-    component::manager::update();
-}
--- mu3controller/src/stdinclude.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-#include <Arduino.h>
-#include <HID-Project.h>
-
-#include "components/manager.hpp"
